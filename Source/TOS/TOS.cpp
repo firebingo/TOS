8842992c3d77822ee7ebfbe83c43dea6dbde5023
Matthew <matthew.t.kides@cox.net>
2015-07-13 16:26:34 -0700

    Recreate Project
    project was recreated to use blueprints as a base instead of c++ code.

--- Source/TOS/TOS.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
-
-#include "TOS.h"
-
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TOS, "TOS" );
-
-DEFINE_LOG_CATEGORY(LogTOS)
- 
